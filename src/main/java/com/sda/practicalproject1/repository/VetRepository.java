62a925bd2b8587adf6698a126fb8b238b4f4de6d
Iulia Ionescu <ionescuiulia5@gmail.com>
2023-03-16 00:23:16 +0200

    PCM-3 create generic hibernate repository

---
+++ src/main/java/com/sda/practicalproject1/repository/VetRepository.java
@@ -0,0 +1,9 @@
+package com.sda.practicalproject1.repository;
+
+import com.sda.practicalproject1.model.Vet;
+import com.sda.practicalproject1.repository.base.Repository;
+
+public interface VetRepository extends Repository<Vet> {
+
+
+}
