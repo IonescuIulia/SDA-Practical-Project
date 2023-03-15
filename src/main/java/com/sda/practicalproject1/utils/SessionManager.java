e4c896e07581786849d92659ad1116f475a1db5a
Iulia Ionescu <ionescuiulia5@gmail.com>
2023-03-15 22:55:16 +0200

    PCM-2 setup hibernate model

--- src/main/java/com/sda/practicalproject1/utils/SessionManager.java
+++ src/main/java/com/sda/practicalproject1/utils/SessionManager.java
@@ -1,23 +1,29 @@
 package com.sda.practicalproject1.utils;
 
+import com.sda.practicalproject1.model.Consult;
+import com.sda.practicalproject1.model.Pet;
+import com.sda.practicalproject1.model.Vet;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class SessionManager extends AbstractSessionManager{
+public class SessionManager extends AbstractSessionManager {
     private static final SessionManager INSTANCE = new SessionManager();
-    private SessionManager(){
+
+    private SessionManager() {
     }
-    public static SessionFactory getSessionFactory()
-    {
+
+    public static SessionFactory getSessionFactory() {
         return INSTANCE.getSessionFactory("pet_clinic1");
     }
 
-    public static void shutdown()
-    {
+    public static void shutdown() {
         INSTANCE.shutdownSessionManager();
     }
+
     @Override
     protected void setAnnotatedClasses(Configuration configuration) {
-
+        configuration.addAnnotatedClass(Vet.class);
+        configuration.addAnnotatedClass(Pet.class);
+        configuration.addAnnotatedClass(Consult.class);
     }
 }
