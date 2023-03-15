cb723497ae9917e47d83006a1d44b8163a968712
Iulia Ionescu <ionescuiulia5@gmail.com>
2023-03-15 21:47:08 +0200

    PCM-1 setup hibernate configuration

--- src/main/java/com/sda/practicalproject1/Main.java
+++ src/main/java/com/sda/practicalproject1/Main.java
@@ -1,7 +1,12 @@
 package com.sda.practicalproject1;
 
+import com.sda.practicalproject1.utils.SessionManager;
+
 public class Main {
     public static void main(String[] args) {
+        SessionManager.getSessionFactory();
 
+
+        SessionManager.shutdown();
     }
 }
